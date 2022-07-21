867c855bda7cb321c1692ae2498208561e37ae1f
Kelvin <kelvinnicolau7@gmail.com>
2022-07-21 00:05:43 -0300

    Header e Rotas prontas

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,11 @@
+function Filme(){
+    return(
+        <div>
+            <h1>
+                Bem vindo a página de Filmes
+            </h1>
+        </div>
+    )
+}
+
+export default Filme;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <div>
+            <h1>
+                Bem vindo a home
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
