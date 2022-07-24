6ff0160534dd962bbc8bdc107f7c82d287593cdc
Kelvin <kelvinnicolau7@gmail.com>
2022-07-24 20:37:12 -0300

    Finalizando projeto - favoritos e alerts atualizad

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './filme-info.css';
+import { toast } from 'react-toastify';
 
 import api from '../../services/api';
 
@@ -48,13 +49,13 @@ function Filme(){
         const hasFilme = filmesSalvos.some( (filmesSalvo) => filmesSalvo.id === filme.id)
 
         if(hasFilme){
-            alert('Esse filme já está na lista!');
+            toast.warn('Esse filme já está na lista!');
             return;
         }
 
         filmesSalvos.push(filme);
         localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
-        alert('Filme salvo com sucesso!');
+        toast.success("Filme salvo com sucesso!");
 
     }
 
